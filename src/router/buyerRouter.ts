b0022375e4ae3074dd30c9a5c122ded07e9bf129
hnin-ei-phyu <nanghnineiphyu2001@gmail.com>
2023-10-11 17:31:15 +0630

    11.10

--- src/router/buyerRouter.ts
+++ src/router/buyerRouter.ts
@@ -1,18 +1,17 @@
 import Buycontroller from "../controllers/buyerController"
 import express from "express"
-import Auth from "../middleware/auth"
 
 const router = express.Router()
 const buyerController = new Buycontroller()
-const auth = new Auth()
 
-router.get("/get-buyer/:id",auth.isBuyer,buyerController.get)
+
+router.get("/get-buyer/:id",buyerController.get)
 router.get("/get-all",buyerController.getAll)
-router.delete("/delete-buyer/:id",auth.isBuyer,buyerController.delete)
-router.put("/update-password/:id",auth.isBuyer,buyerController.updatePassword)
-router.put("/update-info/:id",auth.isBuyer,buyerController.update)
+router.delete("/delete-buyer/:id",buyerController.delete)
+router.put("/update-password/:id",buyerController.updatePassword)
+router.put("/update-info/:id",buyerController.update)
 router.get("/get-paginate",buyerController.paginate)
-router.post("/login-buyer",auth.isBuyer,buyerController.buyerLogin)
+router.post("/login-buyer",buyerController.buyerLogin)
 router.post("/register-buyer",buyerController.buyerRegister)
 router.get("/total-buyer",buyerController.totalCount)
 router.post("/getWithRange",buyerController.getWithRange)
